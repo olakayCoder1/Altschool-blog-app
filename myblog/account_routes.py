d2eb5ebfe52b2dee9fc608ce2fb4207e3a4c2a93
olakayCoder1 <programmerolakay@gmail.com>
2022-11-03 22:15:01 +0100

    first commit

---
+++ myblog/account_routes.py
@@ -0,0 +1,170 @@
+from myblog import app , db , bcrypt
+from flask import render_template , redirect , url_for , flash , request 
+from flask_login import login_user , logout_user , login_required
+from flask_login import login_user , current_user , logout_user , login_required
+from .forms import RegisterForm , LoginForm ,  AccountForm , PasswordResetConfirmForm , PasswordResetRequestForm
+from .models import User , Post 
+from werkzeug.utils import secure_filename
+from myblog.utils import authenticated_not_allowed
+from myblog.utils import MailService  , TokenService 
+import os
+import uuid
+from threading import Thread
+
+
+@app.route('/account')
+@login_required
+def account(): 
+    print("***"*100)
+    print(request.referrer)
+    print("***"*100)  
+    user = User.query.get_or_404(current_user.id)
+    posts = Post.query.filter_by(author=user.id)
+    return render_template('account.html', user=user , posts=posts)
+
+
+@app.route('/account/edit' ,  methods=['POST', 'GET'])
+@login_required
+def account_edit():
+    form = AccountForm()
+    if request.method == 'POST':
+        user = User.query.get(current_user.id)
+        username = request.form.get('username', None)
+        last_name = request.form.get('last_name', None)
+        first_name = request.form.get('first_name', None)
+        email = request.form.get('email', None)
+        image = request.files.get('image', None)
+
+        """
+        check if the updated email is same as the current email 
+        else : check if there is a user with the email
+        """
+        if current_user.email != email :
+            user = User.query.filter_by(email=email).first()
+            if user :
+                flash('Email already exist ', 'error')
+                return render_template('account-edit.html' , form=form)
+        """
+        check if the updated username is same as the current email 
+        else : check if there is a user with the username
+        """
+        if current_user.username != username :
+            user = User.query.filter_by(username=username).first()
+            if user :
+                flash('Username already exist ', 'error')
+                return render_template('account-edit.html' , form=form)
+        
+        print("****"*100)
+        print(image.filename)
+        print("****"*100)
+        if image != None : 
+            filename =  secure_filename(image.filename)
+            name = 'profiles_' + str(uuid.uuid1()) + '_' + filename
+            image_url = url_for('static', filename='uploads/' + name )
+            user.image = image_url
+        try:
+            user.save()
+            if image != None :
+                image.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'] , name))
+        except:
+            db.session.rollback()
+            flash('An error occurred updating ', 'error')
+            return render_template('account-edit.html' , form=form)
+        flash('Account updated successfully', 'success')
+        return redirect(url_for('account'))
+    return render_template('account-edit.html' , form=form)
+
+
+@app.route('/register' , methods=['POST', 'GET'])
+@authenticated_not_allowed
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username_exist = User.query.filter_by(username=form.username.data).first()
+        if username_exist:
+            flash('Username already exist')
+            return render_template('register.html', form=form)
+        email_exist = User.query.filter_by(email=form.email.data).first()
+        if email_exist:
+            flash('Email already exist')
+            return render_template('register.html', form=form)
+        public_id = str(uuid.uuid4().int & (1<<64)-1)
+        new_user = User(username=form.username.data , email=form.email.data , first_name=form.first_name.data , last_name=form.last_name.data , password=form.password1.data , public_id=public_id)
+        try:
+            new_user.save() 
+        except:
+            db.session.rollback()
+        login_user(new_user)
+        return redirect(url_for('posts_page'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login' , methods=['POST', 'GET'])
+@authenticated_not_allowed
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user)
+        if user and user.check_password(form.password.data) :
+            login_user(user)
+            return redirect(url_for('posts_page'))
+        flash('Invalid login credentials')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out. See you next time', category='success')          
+    return redirect(url_for('index'))
+
+
+
+@app.route('/password-reset', methods=['POST', 'GET'])
+def password_reset_email():
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            ## SEND EMAIL TO USER
+            token = TokenService.create_password_reset_token(user.id)
+            public_id=user.public_id
+            Thread(target=MailService.send_reset_mail, kwargs={
+                    'email': user.email ,'token': token , 'public_id':public_id
+                }).start()
+    flash('Instruction to reset your password has been sent to the provided email') 
+    return render_template('password-reset-request.html' , form=form)
+
+
+
+@app.route('/password-reset/<token>/<user_public_id>confirm', methods=['POST', 'GET'])
+def password_reset_confirm(token, user_public_id ):
+    form = PasswordResetConfirmForm()
+    if form.validate_on_submit():
+        password = request.form['password1']
+        confirm_password = request.form['password2']
+        if password and confirm_password :
+            if password == confirm_password :
+                if TokenService.validate_password_token(token, user_public_id ) :
+                    user = User.query.filter_by(public_id=user_public_id ).first()
+                    try:
+                        user.password_hash = password_hash=bcrypt.generate_password_hash(password).decode('utf-8')
+                        flash('Password updated successfully')
+                        return redirect(url_for('login'))
+                    except:
+                        db.session.rollback()
+                        flash('Password reset token is invalid')
+                        return render_template('password-reset-confirm.html' , form=form)
+                flash('Password reset token is invalid')
+                return render_template('password-reset-confirm.html' , form=form)
+            flash('Password does not match')
+            return render_template('password-reset-confirm.html' , form=form)
+    return render_template('password-reset-confirm.html' , form=form)
+
+
+
+
+
---
+++ myblog/post_routes.py
@@ -0,0 +1,115 @@
+from myblog import app , db 
+from flask import render_template , redirect , url_for , flash , request 
+from flask_login import current_user , login_required
+from myblog.models import User , Post
+from werkzeug.utils import secure_filename
+import os
+import uuid
+
+
+
+@app.route('/posts')
+def posts_page():
+    for m in Post.query.all():
+        public_id = uuid.uuid4().int & (1<<64)-1
+        print(current_user)
+        new_post = Post(title='wiowwow', content='sjjsjsjs' , author=1, public_id=public_id)
+        try:
+            new_post.save()
+        except:
+            db.session.rollback()
+        # db.session.delete(m) 
+    # db.session.commit()
+    posts = Post.query.all()
+    users = User.query.all()
+    return render_template('blog.html', posts=posts)
+
+
+@app.route('/posts/<public_id>')
+def post_details_page(public_id):
+    post = Post.query.filter_by(public_id=public_id).first()
+    if post:
+        more = Post.query.filter(Post.id != post.id ).limit(2) 
+        return render_template('blog-details.html', post=post ,  more=more)
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS'] 
+
+@app.route('/posts/create'  , methods=['POST', 'GET'])
+@login_required
+def post_create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        image = request.files.get('image', None)
+        public_id = str(uuid.uuid4().int & (1<<64)-1)
+        new_post = Post(title=title , content=content , author=1, public_id=public_id)
+        print('****'*100)
+        print(len(image.filename))
+        print('****'*100)
+        if len(image.filename) != 0 : 
+            filename =  secure_filename(image.filename)
+            name = 'posts_' + str(uuid.uuid1()) + '_' + filename
+            image_url = url_for('static', filename='uploads/' + name )
+            new_post.image = image_url
+        try:
+            new_post.save() 
+        except:
+            db.session.rollback()   
+            return render_template('editor.html')
+        if len(image.filename) != 0:
+            image.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'] , name))
+        return redirect(url_for('post_details_page', public_id=new_post.public_id)) 
+    return render_template('editor.html')
+
+
+@app.route('/posts/<public_id>/edit'  , methods=['POST', 'GET'])
+@login_required
+def post_edit(public_id):
+    post = Post.query.filter_by(public_id=public_id).first()
+    if post:
+        if request.method == 'POST':
+            post.title = request.form.get('title')
+            post.content = request.form.get('content')
+            image = request.files.get('image', None)  
+            if len(image.filename) != 0 :
+                filename =  secure_filename(image.filename)
+                name = 'posts_' + str(uuid.uuid1()) + '_' + filename
+                image_url = url_for('static', filename='uploads/' + name )
+                post.image = image_url
+            try:
+                post.save()
+                if len(image.filename) != 0 :
+                    image.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'] , name))
+            except:
+                db.session.rollback()
+                flash('An error occurred while updating blog, try again', 'err')
+                return render_template('edit-blog.html', post=post)
+            return redirect(url_for('post_details_page' , public_id=post.public_id ))
+        return render_template('edit-blog.html', post=post)
+    return redirect(url_for('page_not_found'))
+    
+
+
+
+@app.route('/posts/<public_id>/delete')
+@login_required
+def post_delete(public_id):
+    post = Post.query.filter_by(public_id=public_id).first()
+    if post:
+        try:
+            post.delete()
+        except:
+            db.session.rollback()
+    return redirect(url_for('posts_page'))
+
+
+
+
+
+
+
